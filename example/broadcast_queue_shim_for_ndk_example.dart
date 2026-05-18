3742cce23de9deee82ed827e63abbc77366c79ba
Nogringo <npub1kg4sdvz3l4fr99n2jdz2vdxe2mpacva87hkdetv76ywacsfq5leqquw5te@uid.ovh>
2026-05-18 20:57:35 +0200

    first commit

---
+++ example/broadcast_queue_shim_for_ndk_example.dart
@@ -0,0 +1,6 @@
+import 'package:broadcast_queue_shim_for_ndk/broadcast_queue_shim_for_ndk.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/broadcast_queue_shim_for_ndk_test.dart
@@ -0,0 +1,16 @@
+import 'package:broadcast_queue_shim_for_ndk/broadcast_queue_shim_for_ndk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
